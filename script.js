19a146aec2c03bd429ac286e023e9ab27dd25524
anibolokadze <anibolokadze@gmail.com>
2022-10-31 17:33:22 +0400

    game logic

--- script.js
+++ script.js
@@ -11,8 +11,21 @@ let restartSection = document.getElementById('restart');
 let playerWins = document.getElementById('playerWins');
 let tieSection = document.getElementById('tie');
 
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
+
+const cells = document.querySelectorAll('.cell');
+const statusText = document.getElementById('statusText');
+const resetBtn = document.getElementById('resetBtn');
+
+let resultX = document.getElementById("x-player-span");
+let resultO = document.getElementById("o-player-span");
+let options = ["", "", "", "", "", "", "", "", ""];
+
+let x = document.createElement('img');
+x.src = "assets/icon-x.svg";
+let currentPlayer = "X";
+
+let running = true;
+
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -23,147 +36,85 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartBtn = document.getElementById('restartBtn')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
-
-let X_Wrapper = document.getElementsByClassName('.X-wrapper');
-let O_Wrapper = document.getElementsByClassName('.O-wrapper');
-let btn = document.getElementsByClassName('.btn');
-
-
 
 newGameCPU.addEventListener('click', newGame);
 function newGame(){
     startContainer.style.display = "none";
-    newGameCPU.style.display = "inline";
+    newGameCPU.style.display = "block";
     newGamePlayer.style.display = "none";
     boardWrapper.style.display = "block";
-
-    losingSection.style.display = "none";
-    winningSection.style.display = "none";
-    restartSection.style.display = "none";
-    playerWins.style.display = "none";
-    tieSection.style.display = "none";
-}
+};
 newGamePlayer.addEventListener('click', newGameP);
 function newGameP(){
     startContainer.style.display = "none";
     newGameCPU.style.display = "none";
-    newGamePlayer.style.display = "inline";
+    newGamePlayer.style.display = "block";
     boardWrapper.style.display = "block";
 
-    losingSection.style.display = "none";
-    winningSection.style.display = "none";
-    restartSection.style.display = "none";
-    playerWins.style.display = "none";
-    tieSection.style.display = "none";
+    initializeGame();
 }
 
 
+function initializeGame(){
+  cells.forEach(cell => cell.addEventListener('click',cellClicked));
+  resetBtn.addEventListener('click', restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  resultO.innerHTML = 0;
+  resultX.innerHTML = 0;
+}
+function cellClicked(){
+  const cellIndex = this.getAttribute("cellIndex");
+  if(options[cellIndex] != "" || !running){
+    return;
+  } else{
+    updateCell(this, cellIndex);
+    checkWinner();
+  }
+}
+function updateCell(cell, index){
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+}
+function changePlayer(){
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+  let roundWon = false;
+  for(let i = 0; i < WINNING_COMBINATIONS.length; i++){
+    const condition = WINNING_COMBINATIONS[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
 
+    if(cellA == "" || cellB == "" || cellC ==""){
+      continue;
+    }
+    if(cellA == cellB && cellB == cellC){
+      roundWon = true;
+      break;
+    }
+  }
+  if(roundWon){
+    if(`${currentPlayer}` == "X"){
+      resultX.innerHTML ++;
+    } else{
+      resultO.innerHTML ++;
+    }
 
-
-
-// startGame();
-// restartBtn.addEventListener('click',startGame);
-
-// function startGame(){
-//     cellElements.forEach(cell => {
-//         cell.classList.remove(X_CLASS);
-//         cell.classList.remove(CIRCLE_CLASS);
-//         // restartBtn.style.display = "inline";
-//         cell.removeEventListener('click',handleClick);
-//         cell.addEventListener('click', handleClick, {once: true})
-//     });
-//     setBoardHoverClass();
-
-//     winningMessageElement.classList.remove('show');
-// }
-
-// function handleClick(e){
-//     const cell = e.target;
-//     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-    
-//     placeMark(cell, currentClass);
-//     // check for win
-//     if(checkWin(currentClass)){
-//         endGame(false);
-//     } else if(isDraw()){
-//         endGame(true);
-//     } else{
-//         swapTurns();
-//         setBoardHoverClass();
-//     }
-// }
-
-// function placeMark(cell, currentClass){
-//     cell.classList.add(currentClass);
-// }
-// function swapTurns(){
-//     circleTurn = !circleTurn
-// }
-// function setBoardHoverClass(){
-//     board.classList.remove(X_CLASS);
-//     board.classList.remove(CIRCLE_CLASS);
-//     if(circleTurn){
-//         board.classList.add(CIRCLE_CLASS);
-//     } else{
-//         board.classList.add(X_CLASS);
-//     }
-// }
-// function checkWin(currentClass){
-//     return WINNING_COMBINATIONS.some(combinations =>{
-//         return combinations.every(index => {
-//             return cellElements[index].classList.contains(currentClass)
-//         })
-//     })
-// }
-
-// function endGame(draw) {
-//     if (draw) {
-//       winningMessageTextElement.innerText = 'Draw!'
-//     } else {
-//       winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-//     }
-//     winningMessageElement.classList.add('show')
-// }
-// function isDraw(){
-//     return [...cellElements].every(cell => {
-//         return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
-//     })
-// }
-
+    running = false;
+  } else if(!options.includes("")){
+    console.log('draw');
+  } else{
+    changePlayer();
+  }
+}
+function restartGame(){
+  currentPlayer = "X";
+  options = ["", "", "", "", "", "", "", "", ""];
+  statusText.textContent = `${currentPlayer}'s turn`;
+  cells.forEach(cell => cell.textContent = "");
+  running = true;
+}
 
 // https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
