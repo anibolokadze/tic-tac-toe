57d735c8bbe8683f65bfb32f45a5a0ec86859f62
anibolokadze <anibolokadze@gmail.com>
2022-11-02 11:14:29 +0400

    .

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let startContainer = document.getElementById('start-container');
 let newGameCPU = document.getElementById("newGameCPU");
 let newGamePlayer = document.getElementById("newGamePlayer");
 let boardWrapper = document.getElementById('board-wrapper');
-
+let xPlayer = document.getElementsByClassName('.X-wrapper')
+let oPlayer = document.getElementsByClassName('.O-wrapper')
 let restartSection = document.getElementById('restart');
 let playerWins = document.getElementById('playerWins');
 let tieSection = document.getElementById('tie');
@@ -22,10 +23,19 @@ let resetBtn = document.getElementById('resetBtn');
 let resultX = document.getElementById("x-player-span");
 let resultO = document.getElementById("o-player-span");
 let resultTies = document.getElementById('ties-span');
+let playerTitle = document.getElementById('playerTitle');
 
 let options = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
+let currentPlayer;
+
+let xIcon = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>';
+let oIcon = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>';
+
 let running = true;
+
+
+let player;
+
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -47,20 +57,29 @@ function newGameC(){
 
 newGamePlayer.addEventListener('click', newGameP);
 function newGameP(){
-  startContainer.style.display = "none";
-  newGameCPU.style.display = "none";
-  boardWrapper.style.display = "block";
+  // startContainer.style.display = "none";
+  // newGameCPU.style.display = "none";
+  // boardWrapper.style.display = "block";
+
+  
 
   startGame();
 }
 
-
 function startGame(){
   cells.forEach(cell => cell.addEventListener('click',cellClicked));
-  statusText.textContent = `${currentPlayer}'s turn`;
+  statusText.textContent = `${currentPlayer} turn`;
   resultO.innerHTML = 0;
   resultX.innerHTML = 0;
   resultTies.innerHTML = 0;
+
+  if(xPlayer.clicked == true ){
+    console.log('new game');
+  } else if(oPlayer.clicked == true){
+    console.log("O")
+  } else{
+    return null;
+  }
 }
 function cellClicked(){
   const cellIndex = this.getAttribute("cellIndex");
@@ -73,11 +92,11 @@ function cellClicked(){
 }
 function updateCell(cell, index){
   options[index] = currentPlayer;
-  cell.textContent = currentPlayer;
+  cell.innerHTML = currentPlayer === "X" ? xIcon : oIcon;
 }
 function changePlayer(){
   currentPlayer = (currentPlayer == "X") ? "O" : "X";
-  statusText.textContent = `${currentPlayer}'s turn`;
+  statusText.textContent = `${currentPlayer} turn`;
 }
 function checkWinner(){
   let roundWon = false;
@@ -127,7 +146,7 @@ function restart(){
   restartSection.style.display = "none";
   currentPlayer = "X";
   options = ["", "", "", "", "", "", "", "", ""];
-  statusText.textContent = `${currentPlayer}'s turn`;
+  statusText.textContent = `${currentPlayer} turn`;
   cells.forEach(cell => cell.textContent = "");
   running = true;
   resultO.innerHTML = 0;
