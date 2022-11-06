44de3da30319dc261c6b775a30f5302f2811d732
anibolokadze <anibolokadze@gmail.com>
2022-11-06 08:19:57 +0400

    hover

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ let xIcon = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><pat
 let oIcon = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>';
 
 
+
 let running = true;
 
 
@@ -95,10 +96,6 @@ function startGame(){
   resultTies.innerHTML = 0;
 }
 
-
-
-
-
 function cellClicked(){
   const cellIndex = this.getAttribute("cellIndex");
   if(options[cellIndex] != "" || !running){
@@ -111,8 +108,8 @@ function cellClicked(){
 function updateCell(cell, index){
   options[index] = currentPlayer;
   cell.innerHTML = currentPlayer === "X" ? xIcon : oIcon;
-  
 }
+
 function changePlayer(){
   currentPlayer = (currentPlayer == "X") ? "O" : "X";
   statusText.textContent = `${currentPlayer} turn`;
@@ -183,3 +180,4 @@ function nextRound(){
 function quit(){
   window.location.reload();
 }
+
